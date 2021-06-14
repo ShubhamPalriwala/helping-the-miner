ebef24874db4ba04e10ee4de8c1eafe1fa122e40
Shubham Palriwala <spalriwalau@gmail.com>
2021-06-14 18:40:08 +0530

    feat: recd gas fee of 5704530

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <fstream>
+#include <iostream>
 #include <vector>
+#include <map>
 #include <sstream>
+#include <algorithm>
 #define ll long long int
 using namespace std;
 
@@ -12,16 +15,81 @@ struct Transaction
     vector<string> parentTransaction;
 };
 
-vector<Transaction> transactions;
-
-int main()
+struct FeeByWeightAndOriginalIndex
 {
+    double feeByWeightIndividual;
+    int originalIndex;
+};
 
+vector<Transaction> transactions;
+map<string, ll> indexOfId;
+vector<FeeByWeightAndOriginalIndex> feeByWeight;
+vector<Transaction> allIncludedTransactions;
+vector<bool> visited;
+ll totalWeight = 0, totalFee = 0;
+ll maxWeight = 4000000;
+
+bool operator<(FeeByWeightAndOriginalIndex a, FeeByWeightAndOriginalIndex b)
+{
+    return a.feeByWeightIndividual > b.feeByWeightIndividual;
+}
+
+void sortTransactionsWrtWeightByFee()
+{
+    feeByWeight.resize(transactions.size());
+    for (int i = 0; i < feeByWeight.size(); i++)
+    {
+        feeByWeight[i].feeByWeightIndividual = (double(transactions[i].fee) / double(transactions[i].weight));
+        feeByWeight[i].originalIndex = i;
+    }
+    sort(feeByWeight.begin(), feeByWeight.end());
+}
+
+void getTransactionsForGivenConstraints()
+{
+    cout << "Calculating the transactions that are optimal for the given constraints." << endl;
+    sortTransactionsWrtWeightByFee();
+    bool done = false;
+    visited.resize(transactions.size(), false);
+    while (!done)
+    {
+        done = true;
+        for (int i = 0; i < transactions.size(); i++)
+        {
+            if (totalWeight + transactions[feeByWeight[i].originalIndex].weight <= maxWeight && visited[feeByWeight[i].originalIndex] == false)
+            {
+                bool valid = true;
+                for (auto j : transactions[feeByWeight[i].originalIndex].parentTransaction)
+                {
+                    if (!visited[indexOfId[j]])
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid)
+                {
+                    totalWeight += transactions[feeByWeight[i].originalIndex].weight;
+                    totalFee += transactions[feeByWeight[i].originalIndex].fee;
+                    visited[feeByWeight[i].originalIndex] = true;
+                    allIncludedTransactions.push_back(transactions[feeByWeight[i].originalIndex]);
+                    done = false;
+                    break;
+                }
+            }
+        }
+    }
+    cout << "Maximum Possible Fee that can be earned by the miner is: " << totalFee << endl;
+    cout << "The total block weight to get the above fee is: " << totalWeight << endl;
+}
+
+void readMempoolFile()
+{
+    cout << "Reading data from \"mempool.csv\"" << endl;
     ifstream mempoolCsv("mempool.csv");
     string blockData;
 
     getline(mempoolCsv, blockData);
-    // to prevent csv headings from getting stored in our transactions vector
 
     while (getline(mempoolCsv, blockData))
     {
@@ -29,7 +97,7 @@ int main()
         string substr;
         stringstream ss(blockData);
 
-        for (size_t i = 0; i < 4; i++)
+        for (int i = 0; i < 4; i++)
         {
             getline(ss, substr, ',');
             switch (i)
@@ -55,8 +123,30 @@ int main()
                 break;
             }
         }
+        indexOfId[tempTransaction.id] = transactions.size();
         transactions.push_back(tempTransaction);
     }
     mempoolCsv.close();
+    cout << "Data successfully read from \"mempool.csv\"" << endl;
+}
+
+void writeOutputToTxtFile()
+{
+    cout << "Writing transaction IDs to \"block.txt\"" << endl;
+    ofstream outputTransactions("block.txt");
+    for (int i = 0; i < allIncludedTransactions.size(); i++)
+    {
+        outputTransactions << allIncludedTransactions[i].id << endl;
+    }
+    outputTransactions.close();
+    cout << allIncludedTransactions.size() << " have been successfully written to \"block.txt\"" << endl;
+}
+
+int main()
+{
+    readMempoolFile();
+    getTransactionsForGivenConstraints();
+    writeOutputToTxtFile();
+
     return 0;
 }
